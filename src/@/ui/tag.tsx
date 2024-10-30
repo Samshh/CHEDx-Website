673a02890a0e3d26ba55784db6053301af6edbbf
Samshh <samdacs2@gmail.com>
2024-10-30 11:20:24 +0800

    added tags

---
+++ src/@/ui/tag.tsx
@@ -0,0 +1,13 @@
+interface TagProps {
+  children: React.ReactNode;
+}
+
+export default function Tag({children}: TagProps) {
+  return (
+    <div className="inline-block bg-gray-200 rounded-full px-[0.75rem] py-[0.5rem]">
+      <p className="text-[0.75rem]">
+        {children}
+      </p>
+    </div>
+  );
+}
