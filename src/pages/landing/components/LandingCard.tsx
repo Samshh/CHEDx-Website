29bfd9d71e0e51a49bd719a5b6089f9e8dca50ca
{Samshh} <{samdacs2@gmail.com}>
2024-11-01 22:08:11 +0800

    changed font-w8

--- src/pages/landing/components/LandingCard.tsx
+++ src/pages/landing/components/LandingCard.tsx
@@ -11,7 +11,7 @@ export default function LandingCard({ title, icon, number }: LandingCardProps) {
     <div className="text-[#B9B9B9] rounded-2xl border-[2px] border-[#B9B9B9] flex flex-col justify-center items-center transition-all ease-linear hover:scale-105 px-[3rem] py-[3rem] hover:text-yellow hover:border-yellow z-10 select-none">
       <Icon icon={icon} className="text-[3rem] md:text-[4rem]" />
       <h2 className="text-[2.5rem] md:text-[3rem]">{number}</h2>
-      <p className="uppercase">{title}</p>
+      <p className="uppercase font-semibold">{title}</p>
     </div>
   );
 }
