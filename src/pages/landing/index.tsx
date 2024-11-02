8118721792b5e1a8f0446f9470dc17b6d89ba0de
Lolillya <SomeonesUsernam@gmail.com>
2024-11-02 13:52:54 +0800

    ADD: breakout session page dialog

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -8,7 +8,6 @@ import BagongPilipinas from "/landingLogos/BagongPilipinas.svg";
 import PSITS from "/landingLogos/PSITS.svg";
 import CHEDLogo from "/landingLogos/CHEDLogo.svg";
 import CDITE from "/landingLogos/CDITE.svg";
-import LandingCard from "./components/landingCard";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef, useState } from "react";
@@ -16,6 +15,7 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import CurvedUnderline from "/underlines/landing_underline.svg";
 import CurvedUnderline2 from "/underlines/landing2_underline.svg";
 import landingArrow from "/landingArrow.svg";
+import LandingCard from "./components/LandingCard";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -121,11 +121,7 @@ export default function Landing() {
   return (
     <div className="flex flex-col">
       <div className="relative">
-        <img
-          src={HeroBG}
-          alt="herobg"
-          className="absolute w-screen h-full object-cover"
-        />
+        <img src={HeroBG} alt="herobg" className="absolute w-screen h-full object-cover" />
         <div className="absolute inset-0 bg-center-transparent" />
         <section className="relative h-screen">
           <div className="h-full w-full max-w-[568px] max-h-[568px] rounded-full absolute bg-yellow opacity-15 blur-3xl" />
@@ -150,18 +146,10 @@ export default function Landing() {
           <div className="h-full w-full max-w-[568px] max-h-[568px] rounded-full absolute bg-yellow opacity-15 blur-3xl" />
           <div className="max-w-[751px] w-full flex justify-center items-center gap-[3.36rem] flex-wrap">
             {partners.map((partner, index) => (
-              <img
-                className="h-[65px] w-auto"
-                key={index}
-                src={partner}
-                alt={`partner-${index}`}
-              />
+              <img className="h-[65px] w-auto" key={index} src={partner} alt={`partner-${index}`} />
             ))}
           </div>
-          <div
-            className="text-center flex flex-col gap-[2rem]"
-            ref={cardsTrigger}
-          >
+          <div className="text-center flex flex-col gap-[2rem]" ref={cardsTrigger}>
             <h2>
               <span className="text-yellow">What is CHEDx,</span>
               <br /> and why should{" "}
@@ -175,40 +163,23 @@ export default function Landing() {
               </span>
             </h2>
             <h3 className="max-w-[768px]">
-              In today’s fast-evolving tech landscape, CHEDx keeps you informed
-              on ethical implications, promotes responsible use, and fosters
-              collaboration to make technology impactful, bridging gaps and
-              strengthening human connections.
+              In today’s fast-evolving tech landscape, CHEDx keeps you informed on ethical implications, promotes
+              responsible use, and fosters collaboration to make technology impactful, bridging gaps and strengthening
+              human connections.
             </h3>
           </div>
           <div className="grid max-w-[471px] grid-rows-2 grid-cols-2 lg:grid-rows-1 lg:grid-cols-4 gap-[2rem] w-full lg:max-w-[985px]">
             <div ref={card1}>
-              <LandingCard
-                title="speakers"
-                icon="pepicons-pencil:microphone-handheld"
-                number={card1number}
-              />
+              <LandingCard title="speakers" icon="pepicons-pencil:microphone-handheld" number={card1number} />
             </div>
             <div ref={card2}>
-              <LandingCard
-                title="booths"
-                icon="clarity:store-line"
-                number={card2number}
-              />
+              <LandingCard title="booths" icon="clarity:store-line" number={card2number} />
             </div>
             <div ref={card3}>
-              <LandingCard
-                title="breakout sessions"
-                icon="ph:users"
-                number={card3number}
-              />
+              <LandingCard title="breakout sessions" icon="ph:users" number={card3number} />
             </div>
             <div ref={card4}>
-              <LandingCard
-                title="big event"
-                icon="proicons:calendar"
-                number={card4number}
-              />
+              <LandingCard title="big event" icon="proicons:calendar" number={card4number} />
             </div>
           </div>
         </section>
@@ -234,10 +205,9 @@ export default function Landing() {
               </span>
             </h2>
             <h3 className="max-w-[768px]">
-              The summit brings together top experts to foster collaboration,
-              open dialogue, and forward thinking, equipping delegates and
-              industries with the insights and networks to confidently navigate
-              the evolving workplace.
+              The summit brings together top experts to foster collaboration, open dialogue, and forward thinking,
+              equipping delegates and industries with the insights and networks to confidently navigate the evolving
+              workplace.
             </h3>
           </div>
           <div></div>
@@ -259,9 +229,8 @@ export default function Landing() {
               </span>
             </h2>
             <h3 className="max-w-[652px]">
-              From insightful keynotes to engaging breakouts, each session is
-              designed to inspire, educate, and propel you into the forefront of
-              the ever-evolving landscape of technology and education.
+              From insightful keynotes to engaging breakouts, each session is designed to inspire, educate, and propel
+              you into the forefront of the ever-evolving landscape of technology and education.
             </h3>
           </div>
           <div></div>
@@ -271,14 +240,10 @@ export default function Landing() {
         <section className="py-[5rem] gap-[2rem] flex-col-reverse md:flex-row justify-between">
           <div className="flex flex-col items-center md:items-start justify-center gap-[2.5rem]">
             <h2 className="text-center md:text-start">
-              <span className="text-yellow">
-                Exciting things are comming...
-              </span>
+              <span className="text-yellow">Exciting things are comming...</span>
               <br /> Be the first to know what's next
             </h2>
-            <Button className="bg-yellow text-white">
-              Join the mailing list
-            </Button>
+            <Button className="bg-yellow text-white">Join the mailing list</Button>
           </div>
           <img src={landingArrow} alt="venue" />
         </section>
