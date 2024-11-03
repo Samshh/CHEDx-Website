bfc5be0a1168027c5e364da2f8b9f2e5d3067589
Lolillya <SomeonesUsernam@gmail.com>
2024-11-03 18:06:53 +0800

    FIX: breakout sidebar animation

--- src/components/sidebar-components/BreakoutSidebar.tsx
+++ src/components/sidebar-components/BreakoutSidebar.tsx
@@ -8,7 +8,6 @@ import { barToBlack, textToBlack } from "../animations/useSideBar";
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 export default function BreakoutSidebar() {
-  const breakoutRef = useRef<HTMLDivElement>(null);
   const day1Ref = useRef<HTMLDivElement>(null);
   const day2Ref = useRef<HTMLDivElement>(null);
 
@@ -17,16 +16,12 @@ export default function BreakoutSidebar() {
   const day2TextRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
-    const breakout: HTMLElement | null =
-      document.getElementById("breakoutSession");
     const day1: HTMLElement | null = document.getElementById("breakoutDay1");
     const day2: HTMLElement | null = document.getElementById("breakoutDay2");
 
-    textToBlack(breakout, breakoutTextRef);
     textToBlack(day1, day1TextRef);
     textToBlack(day2, day2TextRef);
 
-    barToBlack(breakout, breakoutRef);
     barToBlack(day1, day1Ref);
     barToBlack(day2, day2Ref);
   });
@@ -35,19 +30,13 @@ export default function BreakoutSidebar() {
     <>
       <div className="flex justify-center items-center gap-[1rem] text-black">
         <p ref={breakoutTextRef}>BREAKOUT SESSION</p>
-        <div className="h-[48px] w-[6px] bg-black" ref={breakoutRef} />
+        <div className="h-[48px] w-[6px] bg-black" />
       </div>
-      <div
-        className="flex justify-center items-center gap-[1rem] text-[#777]"
-        ref={day1TextRef}
-      >
+      <div className="flex justify-center items-center gap-[1rem] text-[#777]" ref={day1TextRef}>
         <p>DAY 1</p>
         <div className="h-[48px] w-[6px] bg-[#B9B9B9]" ref={day1Ref} />
       </div>
-      <div
-        className="flex justify-center items-center gap-[1rem] text-[#777]"
-        ref={day2TextRef}
-      >
+      <div className="flex justify-center items-center gap-[1rem] text-[#777]" ref={day2TextRef}>
         <p>DAY 2</p>
         <div className="h-[48px] w-[6px] bg-[#B9B9B9]" ref={day2Ref} />
       </div>
