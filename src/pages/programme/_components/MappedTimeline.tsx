7a2d3eecf133de13b9dfcc16087418e32080e70f
Lolillya <SomeonesUsernam@gmail.com>
2024-11-10 19:19:36 +0800

    EDIT: Timeline details

--- src/pages/programme/_components/MappedTimeline.tsx
+++ src/pages/programme/_components/MappedTimeline.tsx
@@ -27,61 +27,140 @@ const timeline: Timeline = {
       subtitle: "CHED Hymn, VO Introduction of Speakers",
     },
     {
-      time: "9:00 AM - 9:30 AM",
+      time: "9:00 AM - 9:15 AM",
       segment: "Welcome Remarks",
-      title:
-        "Dr. Bonifacio G. Gabales, Jr. & Atty. Cinderella Filipina B. Jaro",
-      subtitle:
-        "SUC President, University of Southeastern Philippines, CHED Executive Director",
+      title: "Dr. Bonifacio G. Gabales, Jr.",
+      subtitle: "SUC IV President, University of Southeastern Philippines",
+    },
+    {
+      time: "9:15 AM - 9:30 AM",
+      segment: "CHEDˣ2.0 : The Road Ahead for Higher Ed",
+      title: "Atty. Cinderella Filipina S. Benitez-Jaro",
+      subtitle: "CHED Executive Director IV",
     },
     {
       time: "9:30 AM - 10:00 AM",
-      segment:
-        "Keynote Speaker: The Role of Higher Education in Producing Futur-Ready IT Professionals",
-      title: "Chairman J. Prospero E. De Vera III",
-      subtitle: "Commission on Higher Education, Ribbon Cutting of Exhibit",
+      segment: "Keynote Speaker: Building Synergy, Aligning with Industry, Leveraging IT Expertise",
+      title: "Secretary J. Prospero E. de Vera III",
+      subtitle: "Commission on Higher Education",
     },
     {
       time: "10:00 AM - 10:30 AM",
-      segment: "X-plor Partnerships and Exhibits",
-      title: "Participants will make their way around the exhibit booths",
-      subtitle: "AM Snacks now available",
+      segment: "Private Sector Advisory Council (PSAC) Jobs for IT Human Resource Development",
+      title: "Mr. Fred Ayala",
+      subtitle: "",
     },
     {
-      time: "10:30 AM - 11:00 AM",
-      segment:
-        "Bringing the industry to the Academe: Conceive Design Implement Operate (CDIO) Framework",
+      time: "10:30 AM - 10:45 AM",
+      segment: "PSF Overview by IBPAP",
+      title: "",
+      subtitle: "",
+    },
+    {
+      time: "10:45 AM - 11:00 AM",
+      segment: "Government-Industry-Academe PSF Commitment Ceremony ",
+      title: "CHED, DICT, IBPAP",
+      subtitle: "",
+    },
+    {
+      time: "11:00 AM - 11:10 AM",
+      segment: "Photo Opportunity",
+      title: "",
+      subtitle: "",
+    },
+    {
+      time: "11:10 AM - 11:30 PM",
+      segment: "Opening of the Exhibits and Ribbon Cutting",
+      title: "Exhibits Tour",
+      subtitle: "",
+    },
+    {
+      time: "11:30 AM - 12: 00",
+      segment: "Press Conference",
+      title: "",
+      subtitle: "",
+    },
+    {
+      time: "12:00 PM - 1:00 PM",
+      segment: "Lunch Break",
+      title: "",
+      subtitle: "",
+    },
+    {
+      time: "1:00 PM - 1:30 PM",
+      segment: "Bringing the Industry to the Academe: Conceive Design Implement Operate (CDIO) Framework",
       title: "Prof Sin Moh Cheah",
       subtitle: "Singapore Polytechnic",
     },
     {
-      time: "11:00 AM - 11:30 AM",
-      segment: "CHED FIRe Framework",
-      title: "Dr. Maricar S. Casquejo",
-      subtitle: "Regional Director, CHED Region XI",
+      time: "1300 PM - 2:15 PM",
+      segment:
+        "Panel Discussion: Education and the World of Work: Boosting Education through Smart Technologies (Project BEST)",
+      title: "Moderator: Calen Martin D. Legaspi, IBPAP TADCOM",
+      subtitle:
+        "Panel Members: CHED OPSD, SKIPP Proponents - Leuther Mojica, Business Head, Orange & Bronze (IBPAP), PSF - Haidee Enriquez (IBPAP), ADDU as Beneficiary",
     },
     {
-      time: "11:30 AM - 12:00 PM",
-      segment: "Panel Discussion c/o CHED/IBPAP",
-      title:
-        "Education and the World of Work: Boosting Education through Smart Technologies (Project BEST)",
-      subtitle: "Calen Martin D. Legaspi, Leuther Mojica, Haidee Enriquez",
+      time: "2:15 PM - 3:00 PM",
+      segment: "BREAKOUT 1: AI for Academic Purpose",
+      title: "c/o IBPAP",
+      subtitle: "Accenture",
     },
     {
-      time: "12:00 PM - 1:30 PM",
-      segment: "Lunch Break & X-plor Partnerships and Exhibits",
+      time: "",
+      segment:
+        "BREAKOUT 2: Investigating Generative AI Mechanisms for the Academe in Improving Educational Engagement and Outcomes",
+      title: "Michelle Banawan, PhD",
+      subtitle: "Academic Program Director, Asian Institute of Management",
+    },
+    {
+      time: "3:00 PM - 3:15 PM",
+      segment: "Tech Bite",
       title: "",
       subtitle: "",
     },
     {
-      time: "1:30 PM - 5:0 PM",
-      segment: "BREAKOUT ROOMS",
-      title: "",
-      subtitle: "",
+      time: "3:15 PM - 4:00 PM",
+      segment:
+        "BREAKOUT 3: The Future of Work: How Digital Transformation Is Changing Workforce Requirements and Skills",
+      title: "Dr. Dodgie Maestrecampo",
+      subtitle: "Mapua University, President",
     },
     {
-      time: "5:00 PM - 5:15 PM",
-      segment: "Synthesis of Day 1 - CHED Regional Office",
+      time: "",
+      segment: "BREAKOUT 4: Women in STEM",
+      title: "Michelle Alarcon c/o IBPAP",
+      subtitle: "President, Analytics Association of the Philippines",
+    },
+    {
+      time: "4:00 PM - 4:45 PM",
+      segment: "BREAKOUT 5: Integrating Soft Skills with Domain Expertise for IT Professionals c/o IBPAP",
+      title: "c/o IBPAP",
+      subtitle: "Industry (iQor)",
+    },
+    {
+      time: "",
+      segment: "BREAKOUT 6: Futures Learning Commons and Creative Spaces, Futures Information",
+      title: "Asso. Prof. Stephen B. Alayon",
+      subtitle: "Program Head, Bachelor of Science and Information Science, West Visayas State University",
+    },
+    {
+      time: "4:45 PM - 5:30 PM",
+      segment:
+        "BREAKOUT 7: Creating Seamless Pathways for Higher Education: Certifications and Microcredentials c/o IBPAP",
+      title: "Ms. Yean Feng Yue",
+      subtitle: "IBM Corporate Social Responsibility Leader – APAC",
+    },
+    {
+      time: "",
+      segment:
+        "BREAKOUT 8: Cybersecurity Synergy: Collaborative Strategies for Academia and Industry in the Evolving Digital Landscape",
+      title: "Allan S. Cabanlong, Asean Eng",
+      subtitle: "Regional Director, GFCE Southeast Asia Hub",
+    },
+    {
+      time: "5:30 PM - 5:45 PM",
+      segment: "Synthesis of Day 1",
       title: "",
       subtitle: "",
     },
@@ -96,62 +175,91 @@ const timeline: Timeline = {
     },
     {
       time: "8:45 AM - 9:15 AM",
-      segment: "PQF and PSF: Supporting the Frameworks c/o IBPAP",
+      segment: "PQF and PSF: Supporting the Frameworks",
       title: "PQF National Coordinating Council, PSF Resource Persons",
       subtitle: "",
     },
     {
-      time: "9:15 AM - 10:0 AM",
+      time: "9:15 AM - 10:00 AM",
       segment: "PSF Foundational Overview c/o IBPAP",
-      title: "DTI PTTC",
-      subtitle: "",
+      title: "c/o IBPAP",
+      subtitle: "DTI PTTC",
     },
     {
-      time: "10:00 AM - 10:15 AM",
-      segment:
-        "Turnover Ceremony of PSF from DICT to Industry Associations c/o IBPAP",
-      title: "USec Jocelle Batapa-Sique",
-      subtitle: "DICT",
+      time: "10:00 AM - 10:30 AM",
+      segment: "PSG Development Based on PSF: The GDA Experience",
+      title: "c/o IBPAP",
+      subtitle: "GDAP",
     },
     {
-      time: "10:45 AM - 11:45 AM",
-      segment: "BREAKOUT ROOMS",
+      time: "10:30 AM - 10:45 AM",
+      segment: "Tech Bite",
       title: "",
       subtitle: "",
     },
     {
-      time: "11:45 AM - 1:30 PM",
-      segment: "Group Photo",
-      title: "Lunch",
-      subtitle: "",
+      time: "10:45 AM - 11:45 PM",
+      segment: "BREAKOUT 9: Contact Center & Business Process Management PSF: Emerging Tech Career Pathways c/o IBPAP",
+      title: "Jay Santisteban - TBD c/o IBPAP",
+      subtitle: "Contact Center Association of the Philippines (CCAP), Operations Director",
     },
     {
-      time: "1:30 PM - 3:00 PM",
-      segment: "BREAKOUT ROOMS",
+      time: "",
+      segment: "BREAKOUT 10: Global In-House Center PSF: Emerging Tech Career Pathway c/o IBPAP",
+      title: "Christine Bata",
+      subtitle: "Global In-House Center Council (GICC), Executive Director",
+    },
+    {
+      time: "11:45 PM - 1:00 PM",
+      segment: "Lunch Break",
       title: "",
       subtitle: "",
     },
     {
-      time: "3:00 PM - 3:15 PM",
-      segment: "PM Snacks",
-      title: "X-plore Partnerships and Exhibits",
+      time: "1:00 PM - 1:45 PM",
+      segment: "BREAKOUT 11: Digital Arts and Animation PSF: Emerging Tech Career Pathways c/o IBPAP",
+      title: "Daniel Enriquez ",
+      subtitle: "Animation Council of the Philippines (ACPI), Executive Director",
+    },
+    {
+      time: "",
+      segment: "BREAKOUT 12: Game Development PSF: Emerging Tech Career Pathways c/o IBPAP",
+      title: "Amierose Ramos",
+      subtitle: "Game Developers Association of the Philippines (GDAP)",
+    },
+    {
+      time: "1:45 PM - 2:30 PM",
+      segment: "BREAKOUT 13: Health Information Management PSF: Emerging Tech Career Pathways c/o IBPAP",
+      title: "Nei Bumanglag - TBD c/o IBPAP",
+      subtitle: "Healthcare Information Management Association of the Philippines, Inc.. (HIMAP), Executive Director",
+    },
+    {
+      time: "",
+      segment: "BREAKOUT 14: Software Development and Security: PSF Emerging Tech Career Pathways c/o IBPAP",
+      title: "Allan Tan",
+      subtitle: "Philippine Software Industry Association (PSIA), Board Trustee",
+    },
+    {
+      time: "2:30 PM - 2:45 PM",
+      segment: "Tech Bite",
+      title: "",
       subtitle: "",
     },
     {
-      time: "3:15 PM - 5:00 PM",
-      segment:
-        "Building Synergy: Aligning IT Education with Industry Demands (panel discussion with audience Q & A)",
-      title: "representing the SUCs & the Industry",
-      subtitle: "",
+      time: "2:45 PM - 4:00 PM",
+      segment: "Building Synergy: Aligning IT Education with Industry Demands (panel discussion with audience Q & A)",
+      title:
+        "Dr. Rolyn Daguil, PhD, President, Caraga State University\nDr. Bobby D. Gerardo, D. Eng, President, Northern Iloilo State University",
+      subtitle: "representing the Industry: \nIBPAP\nPSIA\nISACAA\nModerator: Dr. Gregg Victor Gabison",
     },
     {
-      time: "5:00 PM - 5:15 PM",
+      time: "4:00 PM - 4:15 PM",
       segment: "Covenant Signing",
       title: "",
       subtitle: "",
     },
     {
-      time: "5:15 PM - 5:30 PM",
+      time: "4:15 PM - 4:30 PM",
       segment: "Synthesis of Day 2 - CHED Regional Office",
       title: "",
       subtitle: "",
@@ -163,6 +271,15 @@ const MappedTimeline = () => {
   const days = Object.keys(timeline);
   const timelineRef = useRef(null);
 
+  const formatText = (text: string) => {
+    return text.split("\n").map((line, index) => (
+      <span key={index}>
+        {line}
+        <br />
+      </span>
+    ));
+  };
+
   const res = days.flatMap((day, dayIndex) => (
     <div className="flex w-full" key={day} ref={timelineRef} id={`#${day}`}>
       <div className="text-end flex justify-end w-[25%] pr-10">
@@ -173,41 +290,39 @@ const MappedTimeline = () => {
       </div>
       <div className="flex flex-col w-full">
         {timeline[day].map((event, eventIndex) => (
-          <div
-            className=" w-full gap-3 flex flex-col relative"
-            key={eventIndex}
-          >
+          <div className="w-full gap-3 flex flex-col relative" key={eventIndex}>
             {eventIndex === 0 && (
               <div className="absolute -left-2 -ml-1.5 h-7 w-7 border-blue border bg-white z-10 rounded-full items-center justify-center flex">
                 <div className="h-4 w-4 rounded-full bg-blue z-20"></div>
               </div>
             )}
-            <div className="flex flex-col min-h-40 border-l-[1.5px] border-blue  border-dashed pl-10 z-0 relative">
+            <div className="flex flex-col min-h-40 border-l-[1.5px] border-blue border-dashed pl-10 z-0 relative">
               <div className="h-4 w-4 rounded-full bg-blue z-20 absolute -left-0.5 mt-1.5 -ml-1.5"></div>
-              <div className="min-h-40 hover:pl-10 transition-all duration-300 justify-center flex flex-col w-full timeline-event">
-                <div className="border border-blue bg-none rounded-lg px-2 py-0.5 text-center max-w-max absolute top-0">
-                  <span className="grow-0">{event.time}</span>
-                </div>
+              <div className="min-h-40 hover:pl-10 transition-all duration-300 gap-3 flex flex-col w-full timeline-event">
+                {event.time && (
+                  <div className="border border-blue bg-none rounded-lg px-2 py-0.5 text-center max-w-max top-0">
+                    <span className="grow-0">{event.time}</span>
+                  </div>
+                )}
+
                 <div className="max-w-[568px]">
                   <h3 className="text-blue">{event.segment}</h3>
-                  <h3>{event.title}</h3>
-                  <p>{event.subtitle}</p>
+                  <h3>{formatText(event.title)}</h3>
+                  <p>{formatText(event.subtitle)}</p>
                 </div>
               </div>
-              {dayIndex === days.length - 1 &&
-                eventIndex === timeline[day].length - 1 && (
-                  <Button
-                    onClick={() => console.log("Download Agenda")}
-                    className="absolute -bottom-2 md:-bottom-3 bg-blue text-white flex flex-row"
-                  >
-                    DOWNLOAD AGENDA
-                  </Button>
-                )}
-            </div>
-            {dayIndex === days.length - 1 &&
-              eventIndex === timeline[day].length - 1 && (
-                <div className="absolute -ml-1.5 bottom-0 h-4 w-4 rounded-full bg-blue -left-[1px]"></div>
+              {dayIndex === days.length - 1 && eventIndex === timeline[day].length - 1 && (
+                <Button
+                  onClick={() => console.log("Download Agenda")}
+                  className="absolute -bottom-2 md:-bottom-3 bg-blue text-white flex flex-row"
+                >
+                  DOWNLOAD AGENDA
+                </Button>
               )}
+            </div>
+            {dayIndex === days.length - 1 && eventIndex === timeline[day].length - 1 && (
+              <div className="absolute -ml-1.5 bottom-0 h-4 w-4 rounded-full bg-blue -left-[1px]"></div>
+            )}
           </div>
         ))}
       </div>
