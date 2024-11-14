7a145758c7cc8f09b9f3024d367497089bc34e4c
Samshh <samdacs2@gmail.com>
2024-11-14 13:48:36 +0800

    speakers changes

--- src/pages/landing/components/speakers.tsx
+++ src/pages/landing/components/speakers.tsx
@@ -1,22 +1,69 @@
+
 const speakersData = [
   {
-    name: "Secretary J. Prospero E. de Vera III",
+    name: "J. Prospero E. de Vera III",
     img: "/speakers/chair-popoy.png",
+    tittle: "Chairman",
     affil: "Commission on Higher Education",
   },
   {
     name: "Allan Cabanlong",
     img: "/speakers/allan-canbanlong.jpg",
+    tittle: "Regional Director",
     affil: "GFCE Southeast Asia Hub",
+  },
+  {
+    name: "Virgilio Villarico",
+    img: "/speakers/virgilio-villarico.png",
+    tittle: "Associate Director",
+    affil: "Accenture Operations in the Philippines",
+  },
+  {
+    name: "Rafael Cabredo",
+    img: "/speakers/rafael-cabredo.jpg",
+    tittle: "Board Trustee",
+    affil: "Game Developers Association of the Philippines",
+  },
+  {
+    name: "Haidee Enriquez",
+    img: "/speakers/haidee-enriquez.jpg",
+    tittle: "Chief Executive Officer",
+    affil: "Microsourcing",
+  },
+  {
+    name: "Daniel Enriquez",
+    img: "/speakers/daniel-enriquez.jpg",
+    tittle: "Executive Director",
+    affil: "Animation Council of the Philippines",
+  },
+  {
+    name: "Christine Bata",
+    img: "/speakers/christine-bata.jpg",
+    tittle: "Executive Director",
+    affil: "Global In-House Center Council",
+  },
+  {
+    name: "Dr. Carlos Ongaco",
+    img: "/speakers/carlos-ongaco.jpg",
+    tittle: "Co-Founder and President",
+    affil: "MedCode, Inc. (HIMAP)",
+  },
+  {
+    name: "Calen Legaspi",
+    img: "/speakers/calen-legaspi.png",
+    tittle: "Board Trustee",
+    affil: "Commission on Higher Education",
   },
   {
     name: "Asso. Prof. Stephen Alayon",
     img: "/speakers/stephen-alayon.png",
+    tittle: "Program Head - BSIS",
     affil: "West Visayas State University",
   },
   {
     name: "Dr. Bobby Gerardo",
     img: "/speakers/bobby-gerardo.png",
+    tittle: "President",
     affil: "Northern Iloilo State University",
   },
   {
@@ -27,11 +74,13 @@ const speakersData = [
   {
     name: "Dr. Rolyn Daguil",
     img: "/speakers/rolyn-daguil.png",
+    tittle: "President",
     affil: "Caraga State University",
   },
   {
     name: "Michelle Banawan, PhD",
     img: "/speakers/michelle-banawan.jpg",
+    tittle: "Academic Program Director",
     affil: "Asian Institute of Management",
   },
 ];
