ef50fcce0962e24527886687f1279b8ff22f5294
Samshh <samdacs2@gmail.com>
2024-12-03 13:18:29 +0800

    ADDED: Teaser Video

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -136,7 +136,11 @@ export default function Landing() {
     <div className="flex flex-col">
       {/* section1 */}
       <div className="relative">
-        <img src={HeroBG} alt="herobg" className="absolute w-screen h-full object-cover" />
+        <img
+          src={HeroBG}
+          alt="herobg"
+          className="absolute w-screen h-full object-cover"
+        />
         <div className="absolute inset-0 bg-center-transparent" />
         <section className="relative h-screen">
           <div className="h-full w-full max-w-[568px] max-h-[568px] rounded-full absolute bg-yellow opacity-15 blur-3xl" />
@@ -146,12 +150,20 @@ export default function Landing() {
             </div>
             <div className="flex flex-col justify-center items-center gap-[2rem]">
               <div>
-                <h3 className="text-center font-normal max-w-[568px] uppercase">December 4 - 5, 2024</h3>
+                <h3 className="text-center font-normal max-w-[568px] uppercase">
+                  December 4 - 5, 2024
+                </h3>
                 <p className="text-center font-normal max-w-[568px]">
-                  SMX Convention Center Davao <br /> 3rd Level, SM Lanang Premier, J.P. Laurel Ave, Lanang, Davao City.
+                  SMX Convention Center Davao <br /> 3rd Level, SM Lanang
+                  Premier, J.P. Laurel Ave, Lanang, Davao City.
                 </p>
               </div>
-              <a href="https://lu.ma/sslgryut" target="_blank" rel="noopener noreferrer" title="registration">
+              <a
+                href="https://lu.ma/sslgryut"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="registration"
+              >
                 <Button className="bg-yellow text-white">Register now</Button>
               </a>
             </div>
@@ -164,10 +176,18 @@ export default function Landing() {
           <div className="h-full w-full max-w-[568px] max-h-[568px] rounded-full absolute bg-yellow opacity-15 blur-3xl" />
           <div className="max-w-[751px] w-full flex justify-center items-center gap-[3.36rem] flex-wrap">
             {partners.map((partner, index) => (
-              <img className="h-[65px] w-auto" key={index} src={partner} alt={`partner-${index}`} />
+              <img
+                className="h-[65px] w-auto"
+                key={index}
+                src={partner}
+                alt={`partner-${index}`}
+              />
             ))}
           </div>
-          <div className="text-center flex flex-col gap-[2rem]" ref={cardsTrigger}>
+          <div
+            className="text-center flex flex-col gap-[2rem]"
+            ref={cardsTrigger}
+          >
             <h2>
               <span className="text-yellow">
                 What is CHED<sup>x</sup>2.0,
@@ -183,42 +203,61 @@ export default function Landing() {
               </span>
             </h2>
             <h3 className="max-w-[1080px]" ref={text1}>
-              CHEDˣ2.0 is a nationwide IT Education Summit that promotes impactful and inclusive discussions,
-              partnerships, and networking. This year’s theme, “Building Synergy: Aligning IT Education with Industry,”
-              emphasizes collaboration among academia, industry, and government to keep pace with technological
-              advancements and workforce needs. Join us to connect, learn, and help shape the future of IT education!
+              CHEDˣ2.0 is a nationwide IT Education Summit that promotes
+              impactful and inclusive discussions, partnerships, and networking.
+              This year’s theme, “Building Synergy: Aligning IT Education with
+              Industry,” emphasizes collaboration among academia, industry, and
+              government to keep pace with technological advancements and
+              workforce needs. Join us to connect, learn, and help shape the
+              future of IT education!
             </h3>
           </div>
           <div className="grid max-w-[471px] grid-rows-2 grid-cols-2 lg:grid-rows-1 lg:grid-cols-4 gap-[2rem] w-full lg:max-w-[985px]">
             <div ref={card1}>
-              <LandingCard title="speakers" icon="pepicons-pencil:microphone-handheld" number={card1number} />
+              <LandingCard
+                title="speakers"
+                icon="pepicons-pencil:microphone-handheld"
+                number={card1number}
+              />
             </div>
             <div ref={card2}>
-              <LandingCard title="booths" icon="clarity:store-line" number={card2number} />
+              <LandingCard
+                title="booths"
+                icon="clarity:store-line"
+                number={card2number}
+              />
             </div>
             <div ref={card3}>
-              <LandingCard title="breakout sessions" icon="ph:users" number={card3number} />
+              <LandingCard
+                title="breakout sessions"
+                icon="ph:users"
+                number={card3number}
+              />
             </div>
             <div ref={card4}>
-              <LandingCard title="big event" icon="proicons:calendar" number={card4number} />
+              <LandingCard
+                title="big event"
+                icon="proicons:calendar"
+                number={card4number}
+              />
             </div>
           </div>
         </section>
       </div>
       {/* section3 */}
-      {/* <div className="py-[5rem]">
+      <div className="py-[5rem]">
         <section className="relative">
           <div className="h-full w-full rounded-full absolute bg-yellow opacity-30 blur-3xl" />
           <div className="z-10 h-full w-full overflow-hidden">
             <iframe
               className="w-full h-[50vh] lg:h-[60vh] rounded-2xl border-[2px] border-yellow"
-              src="https://www.youtube.com/embed/cVQJtz2JwRA?si=9i5FxneXHyOhZplv"
+              src="https://www.youtube.com/embed/1XGIHPQ-Ry4?si=UzXJ7B2aQX1jM8M8"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             ></iframe>
           </div>
         </section>
-      </div> */}
+      </div>
       {/* section4 */}
       <div className="py-[5rem]">
         <section className="gap-[4rem]">
@@ -258,9 +297,10 @@ export default function Landing() {
               </span>
             </h2>
             <h3 className="max-w-[768px]" ref={text2}>
-              The summit brings together top experts to foster collaboration, open dialogue, and forward thinking,
-              equipping delegates and industries with the insights and networks to confidently navigate the evolving
-              workplace.
+              The summit brings together top experts to foster collaboration,
+              open dialogue, and forward thinking, equipping delegates and
+              industries with the insights and networks to confidently navigate
+              the evolving workplace.
             </h3>
           </div>
           <SpeakersCarousel />
@@ -285,8 +325,9 @@ export default function Landing() {
               </span>
             </h2>
             <h3 className="max-w-[652px]" ref={text3}>
-              From insightful keynotes to engaging breakouts, each session is designed to inspire, educate, and propel
-              you into the forefront of the ever-evolving landscape of technology and education.
+              From insightful keynotes to engaging breakouts, each session is
+              designed to inspire, educate, and propel you into the forefront of
+              the ever-evolving landscape of technology and education.
             </h3>
           </div>
           <div className="w-full py-[3rem]">
@@ -302,7 +343,12 @@ export default function Landing() {
               <span className="text-yellow">Secure your spot...</span>
               <br /> Join us today!
             </h2>
-            <a href="https://lu.ma/sslgryut" target="_blank" rel="noopener noreferrer" title="registration">
+            <a
+              href="https://lu.ma/sslgryut"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="registration"
+            >
               <Button className="bg-yellow text-white">register now</Button>
             </a>
           </div>
