80eacc319519bf947a4577b8b8a37f2e4f6ceb63
{Samshh} <{samdacs2@gmail.com}>
2024-12-03 16:18:52 +0800

    EDIT: Downloadable Programme

--- src/pages/programme/components/MappedTimeline.tsx
+++ src/pages/programme/components/MappedTimeline.tsx
@@ -310,8 +310,8 @@ const timeline: Timeline = {
     {
       time: "4:31 PM - 5:00 PM",
       segment: "Closing Remarks",
-      title: "Dr. Maricar R. Casquejo, CESO III",
-      subtitle: "Director IV, Commission on Higher Education Regional Office XI",
+      title: "",
+      subtitle: "",
     },
   ],
 };
